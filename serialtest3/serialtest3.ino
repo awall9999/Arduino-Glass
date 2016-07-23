57a5f93fd5110d230733724e470b9a157d1e1b67
Alain Mauer <alain.mauer@vo.lu>
2016-07-23 11:04:01 +0200

    Add files via upload

---
+++ serialtest3/serialtest3.ino
@@ -0,0 +1,13 @@
+
+void setup() {
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+        Serial1.begin(9600);    // opens serial port 1, sets data rate to 9600 bps
+}
+
+void loop() {
+
+  if (Serial1.available())
+    Serial.write(Serial1.read());  //read data from Bluetooth and send it to USB
+  if (Serial.available())
+    Serial1.write(Serial.read());  //read data from USB and send it to Bluetooth 
+}
